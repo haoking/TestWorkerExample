3f6b078ffda88ac5f429267497bef10bad9699ec
Haoking <haokinus@gmail.com>
2018-09-10 12:27:31 -0400

    2
    2

--- testWorker1.js
+++ testWorker1.js
@@ -9,5 +9,11 @@ function getTheData(abc) {
 
 function getTheCV(abc) {
     let aa = cv.matFromArray(3,3,cv.CV_32FC1,abc);
+    
     return aa;
 }
+
+self.addEventListener('message', function (e) {
+    let aa = cv.matFromArray(3,3,cv.CV_32FC1,[1,2,3,4,5,6,7,8,9]);
+  self.postMessage(aa.data32F);
+}, false);
