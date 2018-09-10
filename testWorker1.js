36088546832d3bdc4c1ac7fd6373b2ca8e55fd72
Haoking <haokinus@gmail.com>
2018-09-10 12:42:01 -0400

    4
    4

--- testWorker1.js
+++ testWorker1.js
@@ -1,19 +1,30 @@
 
-importScripts('https://docs.opencv.org/3.4/opencv.js');
+// importScripts('https://docs.opencv.org/3.4/opencv.js');
 
 
 
-function getTheData(abc) {
-    return abc.data32F;
-}
+// function getTheData(abc) {
+//     return abc.data32F;
+// }
 
-function getTheCV(abc) {
-    let aa = cv.matFromArray(3,3,cv.CV_32FC1,abc);
+// function getTheCV(abc) {
+//     let aa = cv.matFromArray(3,3,cv.CV_32FC1,abc);
     
-    return aa;
+//     return aa;
+// }
+
+// self.addEventListener('message', function (e) {
+//     let aa = cv.matFromArray(3,3,cv.CV_32FC1,[1,2,3,4,5,6,7,8,9]);
+//   self.postMessage(aa.data32F);
+// }, false);
+
+
+function timedCount () {
+  for (var i = 0; i < 10000000000; i++) {
+    if (i % 100000 === 0) {
+      postMessage(i);
+    }
+  }
 }
 
-self.addEventListener('message', function (e) {
-    let aa = cv.matFromArray(3,3,cv.CV_32FC1,[1,2,3,4,5,6,7,8,9]);
-  self.postMessage(aa.data32F);
-}, false);
+timedCount();
